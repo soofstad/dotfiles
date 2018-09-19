5b8f9b36fb48fff36ca003609de4da94644ec9a3
stoo <stoo@statoil.com>
2018-09-19 16:28:08 +0200

    feat: added git status in prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,16 @@
-function fish_prompt --description 'Write out the prompt'
-   printf '%s%s@%s%s%s:%s%s%s> ' (set_color yellow) (whoami) (set_color red) (hostname|cut -d . -f 1) (set_color white) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+function fish_prompt --description 'Write out the prompt'     
+   printf '%s' (set_color yellow) (whoami)
+   printf '%s' (set_color normal) (echo '@')
+   printf '%s' (set_color cyan) (hostname)
+   printf '%s' (set_color normal) (echo ':')
+   printf '%s' (set_color white) (__fish_git_prompt)
+   printf '%s' (set_color green)  (prompt_pwd)
+   printf '%s' (set_color normal) (echo '>')
 end
+
