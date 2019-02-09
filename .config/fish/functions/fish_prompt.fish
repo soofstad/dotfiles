afa96308053bf5e3a8857080d8af9adf7e18d772
Stig Ofstad <stigofstad@gmail.com>
2019-02-09 22:32:53 +0100

    git fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,15 +1,13 @@
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
+set __fish_git_prompt_showcolorhints 'yes'
+set __fish_git_prompt_show_informative_status
 
 function fish_prompt --description 'Write out the prompt'     
    printf '%s' (set_color yellow) (whoami)
    printf '%s' (set_color normal) (echo '@')
    printf '%s' (set_color cyan) (hostname)
-   printf '%s' (set_color normal) (echo ':')
+   printf '%s' (set_color normal) (echo '|')
    printf '%s' (set_color white) (__fish_git_prompt)
-   printf '%s' (set_color green)  (prompt_pwd)
-   printf '%s' (set_color normal) (echo '>')
+   printf '%s' (set_color green) (prompt_pwd)
+   printf '%s' (set_color normal) (echo '$')
 end
