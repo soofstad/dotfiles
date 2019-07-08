167eebe91d49222b7bb3647dc0e2f9f223efce86
Stig Ofstad <stigofstad@gmail.com>
2019-07-08 08:11:55 +0200

    fix proxy fish function

--- .config/fish/functions/unset_proxy.fish
+++ .config/fish/functions/unset_proxy.fish
@@ -1,4 +1,6 @@
-set --erase http_proxy
-set --erase https_proxy
-set --erase HTTP_PROXY
-set --erase HTTPS_PROXY
+function unset_proxy --description "Unset all HTTP proxy env vars"
+	set --erase http_proxy
+	set --erase https_proxy
+	set --erase HTTP_PROXY
+	set --erase HTTPS_PROXY
+end
