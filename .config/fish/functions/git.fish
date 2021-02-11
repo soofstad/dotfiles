bc3123c7bff42a4140f47797337f178e82c27c5a
Stig Ofstad <stigofstad@gmail.com>
2021-02-11 12:49:24 +0100

    fish function: git ammend

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -1,3 +1,9 @@
 function clean_branches --description "Deletes all branches not on REMOTE"
 	git fetch -p && git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -D
 end
+
+function gitfix --description "Add changes, do an ammend, and force push"
+	git add -u
+	git commit --amend --reuse-message HEAD
+	git push -f
+end
