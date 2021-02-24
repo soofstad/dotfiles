4946c4c7c07d9f92399bee941f75cae0bbe4e940
Stig Ofstad <stigofstad@gmail.com>
2021-02-24 14:12:48 +0100

    just sync

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -2,7 +2,7 @@ set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_showcolorhints 'yes'
 set __fish_git_prompt_show_informative_status
 
-function fish_prompt --description 'Write out the prompt'     
+function fish_prompt --description 'Write out the prompt'
    printf '%s' (set_color yellow) (whoami)
    printf '%s' (set_color normal) (echo '@')
    printf '%s' (set_color cyan) (hostname)
@@ -10,5 +10,5 @@ function fish_prompt --description 'Write out the prompt'
    printf '%s' (set_color white) (__fish_git_prompt)
    printf '%s' (set_color 3399ff) (__kube_prompt)
    printf '%s' (set_color green) (prompt_pwd)
-   printf '%s' (set_color normal) (echo '$')
+   printf '%s' (set_color brwhite) (echo \U27EB)
 end
